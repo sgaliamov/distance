986f0fe6e20c6a0f6c49366e675f8898da97d67b
s. galiamov <sgalyamov@gmail.com>
2019-01-02 17:16:59 +0100

    created db project

---
+++ Distance.SqlDb/Scripts/test-data.sql
@@ -0,0 +1,17 @@
+﻿DECLARE @Count INT = 1000
+
+WHILE @Count > 0 BEGIN
+
+    DECLARE @Lan NUMERIC (7, 4) = 90 - RAND() * 180
+    DECLARE @Lon NUMERIC (7, 4) = 180 - RAND() * 360
+
+    INSERT [dbo].[Locations] ([Address], [Coordinate]) VALUES
+    (
+        CONCAT('TEST_', @Count),
+        GEOGRAPHY::STPointFromText(CONCAT('POINT(', @Lon, ' ', @Lan, ')'), 4326)
+    )
+
+    SET @Count = @Count - 1
+
+END
+GO
