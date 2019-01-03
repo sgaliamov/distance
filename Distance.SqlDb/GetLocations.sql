a2f9daee1f92c80a60b234e1e0ce6eeb80a355a2
s. galiamov <sgalyamov@gmail.com>
2019-01-03 11:24:57 +0100

    optimise sql

--- Distance.SqlDb/GetLocations.sql
+++ Distance.SqlDb/GetLocations.sql
@@ -1,24 +1,46 @@
 ﻿CREATE PROCEDURE [dbo].[GetLocations]
-    @Longitude NUMERIC(18, 8),
-    @Latitude NUMERIC(18, 8),
+    @Longitude NUMERIC(12, 8),
+    @Latitude NUMERIC(12, 8),
     @Count INT NULL,
     @Distance INT NULL
 AS BEGIN
     DECLARE @Point GEOGRAPHY = CONCAT('POINT(', @Longitude,' ', @Latitude,')')
-    DECLARE @CountStatement VARCHAR(4000) = ''
-    IF @Count IS NOT NULL BEGIN
-        SET @CountStatement = CONCAT('TOP(', @Count, ')')
-    END
-    
-    DECLARE @Statement VARCHAR(4000) = CONCAT(
-        'SELECT ', @CountStatement, ' [Address], 
-            [Coordinate].Long AS [Longitude], [Coordinate].Lat AS [Latitude], 
-            [Coordinate].STDistance(@Point) AS [Distance]
-            FROM [dbo].[Locations]
-            WHERE @Distance IS NULL OR [Coordinate].STDistance(@Point) <= @Distance
-            ORDER BY [Coordinate].STDistance(@Point)')
 
-    EXECUTE sp_executesql @Statement, N'@Distance INT, @Point GEOGRAPHY', @Distance, @Point
+    IF @Count IS NOT NULL AND @Distance IS NOT NULL BEGIN
+        SELECT TOP(@Count)
+                [Address],
+                [Coordinate].Long AS [Longitude],
+                [Coordinate].Lat AS [Latitude],
+                [Coordinate].STDistance(@Point) AS [Distance]
+            FROM [dbo].[Locations]
+            WHERE [Coordinate].STDistance(@Point) <= @Distance
+            ORDER BY [Coordinate].STDistance(@Point)
+    END 
+    ELSE IF @Count IS NULL AND @Distance IS NOT NULL BEGIN
+        SELECT    [Address], 
+                [Coordinate].Long AS [Longitude],
+                [Coordinate].Lat AS [Latitude],
+                [Coordinate].STDistance(@Point) AS [Distance]
+            FROM [dbo].[Locations]
+            WHERE [Coordinate].STDistance(@Point) <= @Distance
+            ORDER BY [Coordinate].STDistance(@Point)
+    END 
+    ELSE IF @Count IS NOT NULL AND @Distance IS NULL BEGIN
+        SELECT TOP(@Count)
+                [Address],
+                [Coordinate].Long AS [Longitude], [Coordinate].Lat AS [Latitude],
+                [Coordinate].STDistance(@Point) AS [Distance]
+            FROM [dbo].[Locations]
+            ORDER BY [Coordinate].STDistance(@Point)
+    END 
+    ELSE IF @Count IS NULL AND @Distance IS NULL BEGIN
+        SELECT    [Address], 
+                [Coordinate].Long AS [Longitude],
+                [Coordinate].Lat AS [Latitude], 
+                [Coordinate].STDistance(@Point) AS [Distance]
+            FROM [dbo].[Locations]
+            ORDER BY [Coordinate].STDistance(@Point)
+    END
 
 END
 GO
--- Distance.SqlDb/Scripts/test-data.sql
+++ Distance.SqlDb/Scripts/test-data.sql
@@ -1,17 +1,18 @@
-﻿DECLARE @Count INT = 1000
+﻿DECLARE @Count INT = 1000000
+DECLARE @Index INT = 0
 
-WHILE @Count > 0 BEGIN
+WHILE @Index < @Count BEGIN
 
     DECLARE @Lan NUMERIC (7, 4) = 90 - RAND() * 180
     DECLARE @Lon NUMERIC (7, 4) = 180 - RAND() * 360
 
     INSERT [dbo].[Locations] ([Address], [Coordinate]) VALUES
     (
-        CONCAT('TEST_', @Count),
+        CONCAT('TEST_', @Index),
         GEOGRAPHY::STPointFromText(CONCAT('POINT(', @Lon, ' ', @Lan, ')'), 4326)
     )
 
-    SET @Count = @Count - 1
+    SET @Index = @Index + 1
 
 END
 GO
