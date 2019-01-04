47cd94a750ad0c79bff4edc7801f29a1ea3db360
s. galiamov <sgalyamov@gmail.com>
2019-01-04 10:07:01 +0100

    added knn implementation

---
+++ distance/Knn/LocationsInMemoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Distance.DataAccess.Entities;
+
+namespace Distance.Knn
+{
+    internal class LocationsInMemoryRepository : ILocationsRepository
+    {
+        public Task<LocationEntity[]> GetLocations(double latitude, double longitude, int? maxDistance, int? maxResults)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<long> AddLocation(double latitude, double longitude, string address)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
